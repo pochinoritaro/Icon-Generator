f308512538583aee9e2526d871dabbdfb302a504
pochimoritaro <pochimoritaro@gmail.com>
2025-07-11 13:18:15 +0900

    ソースフォルダをappからicon_generatorに変更

--- app/generator/git/core/color/__init__.py
+++
@@ -1,9 +0,0 @@
-# __init__.py
-
-# MIT License
-# Copyright (c) 2025 kazuma tunomori
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy...
-from .rgb_generator import RGBGenerator
-
-__all__ = ["RGBGenerator"]
--- app/generator/git/core/pattern/__init__.py
+++
@@ -1,9 +0,0 @@
-# __init__.py
-
-# MIT License
-# Copyright (c) 2025 kazuma tunomori
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy...
-from .pattern_generator import PatternGenerator
-
-__all__ = ["PatternGenerator"]
