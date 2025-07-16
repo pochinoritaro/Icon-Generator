a8b96a826cb130cddb081f04a006185304b7b16d
pochimoritaro <pochimoritaro@gmail.com>
2025-07-16 23:31:57 +0900

    RGBGeneratorの正常系テストひな形を追加

---
+++ tests/generator/git/core/color/test_rgb_generator_positive.py
@@ -0,0 +1,11 @@
+"""RGBGenerator の正常系テストケースを定義するモジュール。"""
+# rgb_generator.py
+
+# MIT License
+# Copyright (c) 2025 kazuma tunomori
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy...
+
+
+class TestRGBGeneratorPositiveCases:
+    """RGBGeneratorにおける正常系の動作を検証するテストクラス。"""
